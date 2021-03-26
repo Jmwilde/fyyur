f55399dcce546e7bbe9b8c072b5c105382d48317
John Wilde <johnwildefire@gmail.com>
2021-03-26 14:20:27 -0700

    Updated README to include documentation of the API endpoints.

--- projects/02_trivia_api/starter/backend/README.md
+++ projects/02_trivia_api/starter/backend/README.md
@@ -76,24 +76,219 @@ One note before you delve into your tasks: for each endpoint, you are expected t
 9. Create error handlers for all expected errors including 400, 404, 422 and 500. 
 
 
+## Endpoints
 
-## Review Comment to the Students
-```
-This README is missing documentation of your endpoints. Below is an example for your endpoint to get all categories. Please use it as a reference for creating your documentation and resubmit your code. 
+`BASE URL: http://localhost:5000`
 
-Endpoints
-GET '/api/v1.0/categories'
-GET ...
-POST ...
-DELETE ...
+The available resources for the API are questions, categories, and quizzes.  
+The following HTTP methods are supported: GET, POST, DELETE.  
 
-GET '/api/v1.0/categories'
+- `GET /categories`
+- `GET /questions`
+- `DELETE /questions/<question_id>`
+- `POST /questions`
+- `GET /categories/<category_id>/questions`
+- `POST /quizzes`
+
+
+### GET /categories
 - Fetches a dictionary of categories in which the keys are the ids and the value is the corresponding string of the category
 - Parameters:
     - `None`
 - Returns:
     - An object with a single key, categories, that contains a object of id: category_string key:value pairs.
 
+```
+curl http://localhost:5000/categories
+
+
+{
+  "categories": {
+    "1": "Science", 
+    "2": "Art", 
+    "3": "Geography", 
+    "4": "History", 
+    "5": "Entertainment",  
+    "6": "Sports"
+  }, 
+  "success": true
+}
+```
+
+### GET /questions
+- Fetches a dictionary with the key "questions" which contains a paginated list of question objects. Each question object has question, answer, category, difficulty, and id fields. The response dictionary also yields all categories and the total number of question objects in the database.
+- Parameters:
+    - `None`
+- Returns:
+    - An object with a single key, categories, that contains a object of id: category_string key:value pairs.
+
+```
+curl http://localhost:5000/questions
+
+
+{
+  "categories": {
+    "1": "Science", 
+    "2": "Art", 
+    "3": "Geography", 
+    "4": "History", 
+    "5": "Entertainment", 
+    "6": "Sports"
+  }, 
+  "current_category": 4, 
+  "questions": [
+    {
+      "answer": "Maya Angelou", 
+      "category": 4, 
+      "difficulty": 2, 
+      "id": 5, 
+      "question": "Whose autobiography is entitled 'I Know Why the Caged Bird Sings'?"
+    }, 
+    {
+      "answer": "Muhammad Ali", 
+      "category": 4, 
+      "difficulty": 1, 
+      "id": 9, 
+      "question": "What boxer's original name is Cassius Clay?"
+    }, 
+    {
+      "answer": "Apollo 13", 
+      "category": 5, 
+      "difficulty": 4, 
+      "id": 2, 
+      "question": "What movie earned Tom Hanks his third straight Oscar nomination, in 1996?"
+    }
+  ], 
+  "success": true, 
+  "total_questions": 3
+}
+```
+
+### DELETE /questions/<question_id>
+- Deletes the question object with the given id.
+- Parameters:
+    - `question_id: integer`
+- Returns:
+    - An object with a single key "success" with a boolean value.
+
+```
+curl -X DELETE http://localhost:5000/questions/1
+
+{
+  "success": true,
+  "message": "Question successfully deleted."
+}
+```
+
+### POST /questions
+- Creates a new question object or performs a case insensitive search for any questions containing a search term.
+- Parameters:
+    - `question: string`  
+    - `answer: string`  
+    - `category: integer (optional)`  
+    - `difficulty: integer (optional)` 
+- Returns:
+    - An object with a single key "success" with a boolean value.
+    - When the "searchTerm" parameter is specified a list of question objects is returned that contain the term within their respective question fields. 
+
+```
+curl -X POST -H "Content-Type: application/json" -d '{"question":"What is the approximate diameter of the Earth?", "answer":"8000 miles", "category":1, "difficulty":2}' http://localhost:5000/questions
+
+{
+  "success": true,
+  "message": "Question successfully created."
+}
+```
+
+Search variant
+```
+curl -X POST -H "Content-Type: application/json" -d '{"searchTerm":"clay"}' http://localhost:5000/questions
+
+{
+  "current_category": 4, 
+  "questions": [
+    {
+      "answer": "Muhammad Ali", 
+      "category": 4, 
+      "difficulty": 1, 
+      "id": 9, 
+      "question": "What boxer's original name is Cassius Clay?"
+    }
+  ], 
+  "success": true, 
+  "total_questions": 1
+}
+```
+
+### GET /categories/<category_id>/questions
+- Fetches a paginated list of question objects that all belong to the specified category.
+- Parameters:
+    - `category_id: integer`
+- Returns:
+    - A paginated list of question objects. Also returns the current category and the total number of questions that belong to the specified category.
+
+```
+curl http://localhost:5000/categories/1/questions?page=1
+
+{
+  "current_category": 1, 
+  "questions": [
+    {
+      "answer": "The Liver", 
+      "category": 1, 
+      "difficulty": 4, 
+      "id": 20, 
+      "question": "What is the heaviest organ in the human body?"
+    }, 
+    {
+      "answer": "Alexander Fleming", 
+      "category": 1, 
+      "difficulty": 3, 
+      "id": 21, 
+      "question": "Who discovered penicillin?"
+    }, 
+    {
+      "answer": "Blood", 
+      "category": 1, 
+      "difficulty": 4, 
+      "id": 22, 
+      "question": "Hematology is a branch of medicine involving the study of what?"
+    }, 
+    {
+      "answer": "8000 miles", 
+      "category": 1, 
+      "difficulty": 2, 
+      "id": 25, 
+      "question": "What is the approximate diameter of the Earth?"
+    }
+  ], 
+  "success": true, 
+  "total_questions": 4
+}
+
+```
+
+### POST /quizzes
+- Fetches the next question object for the trivia quiz. This is a randomly selected question that is not found in the specified previous questions list parameter.
+- Parameters:
+    - `quiz_category: integer (optional)`
+    - `previous_questions: list: integer (optional)`
+- Returns:
+    - A boolean success value and a single question object.
+
+```
+curl -X POST -H "Content-Type: application/json" -d '{"previous_questions":[], "quiz_category":{"type":"Science","id":"1"}}' http://localhost:5000/quizzes
+
+{
+  "question": {
+    "answer": "Blood", 
+    "category": 1, 
+    "difficulty": 4, 
+    "id": 22, 
+    "question": "Hematology is a branch of medicine involving the study of what?"
+  }, 
+  "success": true
+}
 ```
 curl http://localhost:5000/categories
 
--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -19,128 +19,156 @@ def paginate(request, data):
   return data[start:end]
 
 def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  setup_db(app)
-  
-  '''
-  @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
-  '''
-  cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+    # create and configure the app
+    app = Flask(__name__)
+    setup_db(app)
+    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
-  '''
-  @TODO: Use the after_request decorator to set Access-Control-Allow
-  '''
-  @app.after_request
-  def after_request(response):
-      response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-      response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
-      return response
 
-  '''
-  @TODO: 
-  Create an endpoint to handle GET requests 
-  for all available categories.
-  '''
-  @app.route('/categories')
-  def get_categories():
-    try:
-      categories = Category.query.all()
-      formatted_categories = {category.id: category.type for category in categories}
-      return jsonify({
-        'success': True,
-        'categories': formatted_categories
-      })
-    except:
-      abort(404)
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+        response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
+        return response
 
-  '''
-  @TODO: 
-  Create an endpoint to handle GET requests for questions, 
-  including pagination (every 10 questions). 
-  This endpoint should return a list of questions, 
-  number of total questions, current category, categories. 
 
-  TEST: At this point, when you start the application
-  you should see questions and categories generated,
-  ten questions per page and pagination at the bottom of the screen for three pages.
-  Clicking on the page numbers should update the questions. 
-  '''
-  @app.route('/questions')
-  def get_questions():
-    try:
-      questions = Question.query.all()
-      paginated_qs = paginate(request, questions)
+    @app.route('/categories')
+    def get_categories():
+        try:
+            categories = Category.query.all()
+            formatted_categories = {category.id: category.type for category in categories}
+            return jsonify({
+                'success': True,
+                'categories': formatted_categories
+            })
+        except:
+            abort(400)
 
-      categories = Category.query.all()
-      formatted_categories = {category.id: category.type for category in categories}
-      return jsonify({
-        'success': True,
-        'questions': [q.format() for q in paginated_qs],
-        'total_questions': len(questions),
-        'categories': formatted_categories,
-        'current_category': paginated_qs[0].category
-      })
-    except:
-      abort(404)
 
-  '''
-  @TODO: 
-  Create an endpoint to DELETE question using a question ID. 
+    @app.route('/questions')
+    def get_questions():
+        try:
+            questions = Question.query.all()
+            paginated_qs = paginate(request, questions)
 
-  TEST: When you click the trash icon next to a question, the question will be removed.
-  This removal will persist in the database and when you refresh the page. 
-  '''
-  @app.route('/questions/<int:q_id>', methods=['DELETE'])
-  def delete_question(q_id):
-    try:
-      question = Question.query.get(q_id)
-      if not question:
-        abort(404)
+            categories = Category.query.all()
+            formatted_categories = {category.id: category.type for category in categories}
+            return jsonify({
+                'success': True,
+                'questions': [q.format() for q in paginated_qs],
+                'total_questions': len(questions),
+                'categories': formatted_categories,
+                'current_category': paginated_qs[0].category
+            })
+        except:
+            abort(400)
 
-      question.delete()
-      return jsonify({
-        'success': True
-      })
-    except:
-      abort(422)
 
-  '''
-  @TODO: 
-  Create an endpoint to POST a new question, 
-  which will require the question and answer text, 
-  category, and difficulty score.
+    @app.route('/questions/<int:q_id>', methods=['DELETE'])
+    def delete_question(q_id):
+        try:
+            question = Question.query.get(q_id)
+            if not question:
+                raise ValueError('Question with id {} does not exist.'.format(q_id))
+            question.delete()
+            return jsonify({
+                'success': True,
+                'message': 'Question successfully deleted.'
+            })
+        except ValueError:
+            abort(422)
+        except:
+            abort(400)
 
-  TEST: When you submit a question on the "Add" tab, 
-  the form will clear and the question will appear at the end of the last page
-  of the questions list in the "List" tab.  
-  '''
 
-  '''
-  @TODO: 
-  Create a POST endpoint to get questions based on a search term. 
-  It should return any questions for whom the search term 
-  is a substring of the question. 
+    @app.route('/questions', methods=['POST'])
+    def create_question():
+        try:
+            body = json.loads(request.data)
+            new_question = body.get('question', None)
+            new_answer = body.get('answer', None)
+            new_category = body.get('category', None)
+            new_difficulty = body.get('difficulty', None)
+            search = body.get('searchTerm', None)
 
-  TEST: Search by any phrase. The questions list will update to include 
-  only question that include that string within their question. 
-  Try using the word "title" to start. 
-  '''
-  @app.route('/questions', methods=['POST'])
-  def create_or_search_question():
-    body = json.loads(request.data)
-    new_question = body.get('question', None)
-    new_answer = body.get('answer', None)
-    new_category = body.get('category', None)
-    new_difficulty = body.get('difficulty', None)
-    search = body.get('searchTerm', None)
+            if search:
+                results = Question.query.filter(Question.question.ilike('%{}%'.format(search))).all()
+                questions = paginate(request, results)
+                formatted_qs = [q.format() for q in questions]
+            
+                return jsonify({
+                    'success': True,
+                    'questions': formatted_qs,
+                    'total_questions': len(results),
+                    'current_category': questions and questions[0].category
+                })
+            else:
+                if not (new_question and new_answer):
+                    raise ValueError('Both question and answer fields must not be None.')
+                question = Question(question=new_question, answer=new_answer, category=new_category, difficulty=new_difficulty)
+                question.insert()
+                return jsonify({
+                    'success': True,
+                    'message': 'Question successfully created.'
+                })
+        except JSONDecodeError:
+            abort(400)
+        except Exception as e:
+            abort(422)
+            print(e)
 
-    try:
-      if search:
-        results = Question.query.filter(Question.question.ilike('%{}%'.format(search))).all()
-        questions = paginate(request, results)
-        formatted_qs = [q.format() for q in questions]
-        
+
+    @app.route('/categories/<int:category_id>/questions')
+    def get_questions_for_category(category_id):
+        try:
+            results = Question.query.filter(Question.category == category_id).all()
+            questions = paginate(request, results)
+            formatted_qs = [q.format() for q in questions]
+            return jsonify({
+                'success': True,
+                'questions': formatted_qs,
+                'total_questions': len(results),
+                'current_category': questions and questions[0].category
+            })
+        except:
+            abort(400)
+
+
+    @app.route('/quizzes', methods=['POST'])
+    def get_quiz_question():
+        try:
+            body = json.loads(request.data)
+            previous = body.get('previous_questions', [])
+            category = body.get('quiz_category', None)
+
+            if category:
+                category_id = category.get('id', 0)
+                questions = Question.query.filter(Question.id.notin_(previous), Question.category == category_id).all()
+            else:
+                questions = Question.query.filter(Question.id.notin_(previous)).all()
+            
+            question = random.choice(questions).format() if questions else None
+            return jsonify({
+                'success': True,
+                'question': question,
+            })
+        except JSONDecodeError:
+            abort(400)
+        except:
+            abort(422)
+
+
+    @app.errorhandler(400)
+    def bad_request(e):
+        return jsonify({
+            'succes': False,
+            'error': 400,
+            'message': 'Request was not formatted correctly.'
+        }), 400
+
+
+    @app.errorhandler(404)
+    def not_found(e):
         return jsonify({
           'success': True,
           'questions': formatted_qs,
